1d57690594440333d309e3a1ff944df14a4aa66e
LENOVO <LENOVO@LENOVO-PC-3>
2024-02-26 15:39:25 +0530

    COMMIT

---
+++ ArithmeticOperator/src/arithmatic/ArithmaticOperator.java
@@ -0,0 +1,12 @@
+package arithmatic;
+
+public class ArithmaticOperator {
+
+	public static void main(String[] args) {
+		
+		int i=10,j=5;
+		System.out.println("Addition: "+(i+j)+"\nSubtraction: "+(i-j));
+
+	}
+
+}
